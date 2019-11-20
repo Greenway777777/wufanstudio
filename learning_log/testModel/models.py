e521ab7ee071c7d3692b66eeb4b0414dcfe8d641
livre <livre@haomingdeiMac.lan>
2019-11-20 11:31:10 +0800

    10.20

--- learning_log/testModel/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserInfo(models.Model):
-    name=models.CharField(max_length=20)
-    sex=models.CharField(max_length=10)
-    age=models.IntegerField()
-    group=models.CharField(max_length=4)
-    idpassword=models.CharField(max_length=19,unique=True)
