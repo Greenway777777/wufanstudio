2d62d4c399935e27603f3bc43ebc16cf410c6a33
livre <livre@haomingdeiMac.lan>
2019-11-20 12:18:53 +0800

    11.20

---
+++ HelloWorld/HelloWorld/testdb.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+from TestModel.models import TestmodelUserinfo
+
+# 数据库操作
+def testdb(request):
+  test1 = TestmodelUserinfo(name='tom', sex='male', age=21, group='2017', idpassword='440923199803145733')
+  test1.save()
+  return HttpResponse("<p>数据添加成功！</p>")
+
+
+# 数据库操作
+def testdb2(request):
+    # 初始化
+    response = ""
+    response1 = ""
+    # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
+    list = TestmodelUserinfo.objects.all()
+
+    # filter相当于SQL中的WHERE，可设置条件过滤结果
+    #response2 = TestmodelUserinfo.objects.filter(id=1)
+
+    # 获取单个对象
+    #response3 = TestmodelUserinfo.objects.get(id=1)
+
+    # 限制返回的数据 相当于 SQL 中的 OFFSET 0 LIMIT 2;
+    #TestmodelUserinfo.objects.order_by('name')[0:2]
+
+    # 数据排序
+    #TestmodelUserinfo.objects.order_by("id")
+
+    # 上面的方法可以连锁使用
+    #TestmodelUserinfo.objects.filter(name="w3cschool.cn").order_by("id")
+
+    # 输出所有数据
+    for var in list:
+        response1 += var.name + " "
+        response = response1
+    return HttpResponse("<p>" + response + "</p>")
+
+
+# 数据库操作
+def testdb3(request):
+    # 修改其中一个id=1的name字段，再save，相当于SQL中的UPDATE
+    test1 = TestmodelUserinfo.objects.get(id=1)
+    test1.name = 'w3cschoolW3Cschool教程'
+    test1.save()
+    # 另外一种方式
+    # Test.objects.filter(id=1).update(name='w3cschoolW3Cschool教程')
+
+    # 修改所有的列
+    # Test.objects.all().update(name='w3cschoolW3Cschool教程')
+    return HttpResponse("<p>修改成功</p>")
+
+
+# 数据库操作
+def testdb4(request):
+    # 删除id=1的数据
+    test1 = TestmodelUserinfo.objects.get(id=1)
+    test1.delete()
+    # 另外一种方式
+    # Test.objects.filter(id=1).delete()
+
+    # 删除所有数据
+    # Test.objects.all().delete()
+    return HttpResponse("<p>删除成功</p>")
--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -1,4 +1,4 @@
-"""HelloWord URL Configuration
+"""HelloWorld URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]"""
+from django.conf.urls import url
+from HelloWorld.view import hello
+from HelloWorld.testdb import testdb
+
+urlpatterns = [
+        url('^hello/$', hello),
+        url('^testdb/$', testdb),
 ]
